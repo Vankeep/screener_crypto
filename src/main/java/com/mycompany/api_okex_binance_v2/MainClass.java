b92d3410a3132cbe109d091660e7b5a1133d0f19
ivan mashkov <ivan@mashkof.ru>
2021-11-04 18:32:03 +0300

    ADD: ALL: Загрузка пар с биржи и перенос их в БД в двух потоках

--- src/main/java/com/mycompany/api_okex_binance_v2/MainClass.java
+++ src/main/java/com/mycompany/api_okex_binance_v2/MainClass.java
@@ -6,6 +6,8 @@ import com.mycompany.api_okex_binance_v2.database.Database;
 import com.mycompany.api_okex_binance_v2.database.GenerateSqlMessage;
 import com.mycompany.api_okex_binance_v2.net.Connect;
 import com.mycompany.api_okex_binance_v2.time.Time;
+import java.io.IOException;
+import java.nio.CharBuffer;
 
 public class MainClass {
 
@@ -16,62 +18,36 @@ public class MainClass {
     public String[] close;
     public String[] volume;
     CoinBigData name;
+    boolean updateOkex = false;
+    boolean updateBinance = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MainClass mainClass = new MainClass();
         mainClass.momo();
 
     }
-    
-    public void momo() {
-//        GenerateSqlMessage gsm = new GenerateSqlMessage();
-//        time = new String[]{"1635747564", "1635747564", "1635747564", "1635747564"};
-//        open = new String[]{"0.000541", "0.000881", "0.000798", "0.000715"};
-//        high = new String[]{"0.000542", "0.000882", "0.000798", "0.000715"};
-//        low = new String[]{"0.000543", "0.000883", "0.000798", "0.000715"};
-//        close = new String[]{"0.000544", "0.000884", "0.000798", "0.000715"};
-//        volume = new String[]{"12.23", "13.44", "0.000798", "0.000715"};
-//        
-//        CoinBigData dodo = new CoinBigData("DODO", time, open, high, low, close, volume, ConstCoin.BTC);
-//        CoinBigData slp = new CoinBigData("SLP", time, open, high, low, close, volume, ConstCoin.BTC);
-//        
-//       
-//        GenerateUrlMessage gumOK = new GenerateUrlMessage(ConstExchange.EX_OKEX);
-//        GenerateUrlMessage gumBN = new GenerateUrlMessage(ConstExchange.EX_BINANCE);
-//        
-//        System.out.println(gumOK.getCoinData("BTC", ConstCoin.USDT, ConstTF.HOUR_ONE));
-//        System.out.println(gumBN.getCoinData("BTC", ConstCoin.USDT, ConstTF.HOUR_ONE));
 
-        //Тру способ получить данные 
-        /*
-        Connect connectBinance = new Connect(ConstExchange.EX_BINANCE);
-        ArrayList<ArrayList<String>> list = connectBinance.getAllExInfo();
-        if (list != null) {
-            for (int i = 0; i < 3; i++) {
-                System.out.println(list.get(i).toString());
-            }
-        } else {
-            System.out.println("Ошибка FileNotFoundException");
-        }
-        */
-        boolean ok = false;
+    public void momo() throws InterruptedException {
         ApiClient okex = new Connect(Const.Exchange.EX_OKEX);
-        ok = okex.updateAllPair();
-        if (ok) {
-            System.out.println("Данные в базе");
-        } else {
-            System.out.println("Ничего не записалось");
-        }
         ApiClient binance = new Connect(Const.Exchange.EX_BINANCE);
-        ok = binance.updateAllPair();
-        if(ok){
-            System.out.println("Данные в базе");
-        }else{
-            System.out.println("Ничего не записалось");
-        }
-        
-        
+
+        Thread one = new Thread(() -> {
+            if (binance.updateAllPair()) {
+                updateBinance = true;
+            }
+        });
+        one.start();
+        Thread two = new Thread(() -> {
+            if (okex.updateAllPair()) {
+                updateOkex = true;
+            }
+        });
+        two.start();
+        one.join();
+        two.join();
+         
         
 
     }
+
 }
